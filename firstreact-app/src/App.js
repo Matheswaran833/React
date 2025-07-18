b5c2bfddf42ee754d33d47d52c058456ca4f1170
Matheswaranp <mathesswaran@gmail.com>
2025-07-18 21:29:41 +0200

    React first

---
+++ firstreact-app/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello React!</h1>
+      <p>This is my first component.</p>
+      <button onClick={() => alert("Welcome!")}>
+        Click Me
+      </button>
+    </div>
+  );
+}
+
+export default App;
